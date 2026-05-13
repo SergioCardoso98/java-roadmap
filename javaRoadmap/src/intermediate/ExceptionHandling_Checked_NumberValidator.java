f8b84c666ffd148ad665056370ed935f8d0803f9
Abilio Fonseca <a.s.cardoso1998@gmail.com>
2026-05-13 17:53:24 +0100

    exceptio handling ongoing

---
+++ javaRoadmap/src/intermediate/ExceptionHandling_Checked_NumberValidator.java
@@ -0,0 +1,6 @@
+package intermediate;
+
+public class ExceptionHandling_Checked_NumberValidator extends Exception{
+    public ExceptionHandling_Checked_NumberValidator(){super();};
+    public ExceptionHandling_Checked_NumberValidator(String errorMessage){super(errorMessage);}
+}
